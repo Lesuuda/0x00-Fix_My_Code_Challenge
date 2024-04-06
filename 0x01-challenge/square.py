76c7c2a0069d79dcc874c5208714a5a0949b2385
lesuuda <lesuudaljamani@gmail.com>
2024-04-06 22:48:16 +0300

    fix: pycodestyle and documentation for square.py

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
+""" class that describes a square"""
+
 
 class Square():
-    
+    """represents a square"""
+
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
+        """initializes the square"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -15,10 +18,13 @@ class Square():
         return self.width * self.height
 
     def perimiter_of_my_square(self):
+        """ Perimiter of the square"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """string representation of the square"""
         return "{}/{}".format(self.width, self.height)
+
 
 if __name__ == "__main__":
 
